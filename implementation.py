8b9e60ea885cf55aa45ad1944b489f64830f6da8
Aaron Ge <aaronge13@icloud.com>
2023-11-20 13:12:55 -0500

    K-fold-CV

--- implementation.py
+++ implementation.py
@@ -1,155 +1,127 @@
-def create_mask_from_json(json_data, shape):
-    mask = np.zeros(shape, dtype=np.float32)
-    for item in json_data:
-        rr, cc = disk((item['y'], item['x']), 0, shape=shape)  # '16' is an arbitrary radius for the core
-        mask[rr, cc] = 1.0
-    return mask
+import tensorflow as tf
+import numpy as np
+import json
+import os
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-def load_images_and_labels(image_dir, label_dir):
+# Function to resize labels
+def resize_labels(labels, original_size, new_size):
+    # Adjust the label coordinates for the new image size
+    scale_x = new_size[1] / original_size[1]
+    scale_y = new_size[0] / original_size[0]
+    resized_labels = []
+    for label in labels:
+        resized_label = {
+            'x': label['x'] * scale_x,
+            'y': label['y'] * scale_y,
+            'radius': label['radius'] * scale_x  # Assuming uniform scaling in x and y
+        }
+        resized_labels.append(resized_label)
+    return resized_labels
+
+
+# Function to convert labels to EfficientDet format
+def convert_to_efficientdet_format(labels, image_shape):
+    efficientdet_labels = []
+    for label in labels:
+        # Calculate the coordinates of the upper left and lower right corners
+        xmin = (label['x'] - label['radius']) / image_shape[1]
+        ymin = (label['y'] - label['radius']) / image_shape[0]
+        xmax = (label['x'] + label['radius']) / image_shape[1]
+        ymax = (label['y'] + label['radius']) / image_shape[0]
+        # EfficientDet format [ymin, xmin, ymax, xmax]
+        efficientdet_labels.append([
+            max(0, ymin), max(0, xmin), min(1, ymax), min(1, xmax)
+        ])
+    return efficientdet_labels
+
+
+# Function to load images and labels
+def load_images_and_labels(image_dir, label_dir, original_size=(1024, 1024), new_size=(512, 512)):
     image_files = [os.path.join(image_dir, file) for file in sorted(os.listdir(image_dir)) if file.endswith('.png')]
     label_files = [os.path.join(label_dir, file) for file in sorted(os.listdir(label_dir)) if file.endswith('.json')]
     
     images = []
-    masks = []
+    all_boxes = []
 
     for image_file, label_file in zip(image_files, label_files):
-        # Load image
-        image = img_to_array(load_img(image_file, color_mode='rgb'))  # or 'rgb' if your images are colored
-        images.append(image / 255.0)  # Normalizing to [0, 1]
+        # Load and resize image
+        image = load_img(image_file, color_mode='rgb', target_size=new_size)
+        # Convert the image to an array and scale the pixel values to [-1, 1]
+        image = (img_to_array(image) / 127.5) - 1
+        images.append(image)
 
-        # Load corresponding label
+        # Load labels and adjust for new image size
         with open(label_file, 'r') as file:
             json_data = json.load(file)
-        mask = create_mask_from_json(json_data, shape=(1024, 1024))
-        masks.append(mask)
+        resized_json_data = resize_labels(json_data, original_size=original_size, new_size=new_size)
+        boxes = convert_to_efficientdet_format(resized_json_data, new_size)
+        all_boxes.append(boxes)
+    
+    return np.array(images), all_boxes
 
-    return np.array(images), np.array(masks).reshape(-1, 1024, 1024, 1)
 
-# Usage
+# Function to pad labels to a fixed size
+def pad_labels(labels, max_boxes=250, pad_value=0):
+    padded_labels = []
+    for label in labels:
+        padded_label = np.zeros((max_boxes, 4), dtype=np.float32) + pad_value
+        num_boxes = min(len(label), max_boxes)
+        padded_label[:num_boxes] = label[:num_boxes]
+        padded_labels.append(padded_label)
+    return np.array(padded_labels)
+
+# Function to prepare the dataset
+def prepare_dataset(images, boxes, batch_size, num_boxes=250):
+    images = tf.constant(images, dtype=tf.float32)
+    boxes = pad_labels(boxes, max_boxes=num_boxes)
+    boxes = tf.constant(boxes, dtype=tf.float32)
+
+    dataset = tf.data.Dataset.from_tensor_slices((images, boxes))
+    dataset = dataset.shuffle(len(images)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    return dataset
+
+batch_size = 2
 image_dir = './TMA_WSI_Padded_PNGs'
 label_dir = './TMA_WSI_Labels_updated'
-images, masks = load_images_and_labels(image_dir, label_dir)
+images, boxes = load_images_and_labels(image_dir, label_dir)
+# Map this preprocessing function to your dataset
 
-from tensorflow.keras.layers import LeakyReLU
 
-def conv_block(input_tensor, num_filters, kernel_size=3, do_batch_norm=True, leaky_relu_alpha=0.01):
-    # A conv block consists of two convolutions, each followed by a batch normalization and a Leaky ReLU activation.
-    x = Conv2D(num_filters, kernel_size, padding='same', kernel_initializer='he_normal')(input_tensor)
-    if do_batch_norm:
-        x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=leaky_relu_alpha)(x)
+# Assuming we have only one class for object detection and 100 boxes per image
+num_boxes = 250  # Maximum number of boxes per image, adjust as needed
+
+dataset = prepare_dataset(images, boxes, batch_size, num_boxes=num_boxes)
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+def create_object_detection_model(num_boxes):
+    backbone = tf.keras.applications.MobileNetV2(input_shape=[512, 512, 3], include_top=False, weights='imagenet')
+    backbone.trainable = False
+
+    x = backbone.output
+    x = layers.Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same')(x)
+    x = layers.Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same')(x)
+    x = layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+
+    # Output layer for bounding box predictions
+    # The output is reshaped to [batch_size, num_boxes, 4]
+    boxes_output_flat = layers.Dense(num_boxes * 4, activation='sigmoid', name='boxes_output')(x)
+    boxes_output = layers.Reshape((num_boxes, 4))(boxes_output_flat)
+
+    model = models.Model(inputs=backbone.input, outputs=boxes_output)
     
-    x = Conv2D(num_filters, kernel_size, padding='same', kernel_initializer='he_normal')(x)
-    if do_batch_norm:
-        x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=leaky_relu_alpha)(x)
-    return x
-
-def unet(input_size=(1024, 1024, 3), num_filters=32, depth=3, dropout=0.5, batch_norm=True, leaky_relu_alpha=0.01):
-    # INPUT LAYER
-    inputs = Input(input_size)
-    # CONTRACTING PATH
-    conv_blocks = []
-    x = inputs
-    for i in range(depth):
-        x = conv_block(x, num_filters * (2**i), do_batch_norm=batch_norm, leaky_relu_alpha=leaky_relu_alpha)
-        conv_blocks.append(x)
-        x = MaxPooling2D(pool_size=(2, 2))(x)
-        if dropout:
-            x = Dropout(dropout)(x)
-
-    # BOTTLENECK
-    x = conv_block(x, num_filters * (2**(depth)), do_batch_norm=batch_norm, leaky_relu_alpha=leaky_relu_alpha)
-    
-    # EXPANSIVE PATH
-    for i in reversed(range(depth)):
-        num_filters_exp = num_filters * (2**i)
-        x = UpSampling2D(size=(2, 2))(x)
-        x = concatenate([x, conv_blocks[i]], axis=3)
-        x = conv_block(x, num_filters_exp, do_batch_norm=batch_norm, leaky_relu_alpha=leaky_relu_alpha)
-
-    # FINAL CONVOLUTION
-    output = Conv2D(1, 1, activation='sigmoid')(x)
-    model = Model(inputs=inputs, outputs=output)
-
     return model
 
 
+model = create_object_detection_model(num_boxes)
 
-from tensorflow.keras.optimizers.legacy import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
-from tensorflow.keras.models import load_model
-from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, LearningRateScheduler
+import datetime
 import os
 
-from tensorflow.keras.optimizers.legacy import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
-from tensorflow.keras.models import load_model
-from tensorflow.keras import backend as K
-import os
-
-from tensorflow.keras.callbacks import LearningRateScheduler
-
-# Define a learning rate schedule (example: decrease learning rate by half every 10 epochs)
-def lr_schedule(epoch, lr):
-    if epoch % 5 == 0 and epoch != 0:
-        lr = lr / 2
-    return lr
-
-# Add this to your callbacks in the training function
-lr_scheduler = LearningRateScheduler(lr_schedule, verbose=1)
-
-
-log_dir = "./tensorboard_logs"
-
-
-def focal_loss(gamma=2., alpha=.25):
-    def focal_loss_fixed(y_true, y_pred):
-        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-
-        # Clip the predicted values to prevent log(0) error.
-        eps = 1e-12
-        pt_1 = tf.clip_by_value(pt_1, eps, 1. - eps)
-        pt_0 = tf.clip_by_value(pt_0, eps, 1. - eps)
-
-        return -tf.reduce_sum(alpha * tf.pow(1. - pt_1, gamma) * tf.math.log(pt_1)) \
-               -tf.reduce_sum((1 - alpha) * tf.pow(pt_0, gamma) * tf.math.log(1. - pt_0))
-
-    return focal_loss_fixed
-
-# You would then compile your model with this loss, adjusting the weights as needed for your imbalance
-def train_unet(model, images, masks, epochs=20, batch_size=3, checkpoint_path='pixel_cores.hdf5'):
-    
-    # Define the custom loss function
-    custom_loss = focal_loss(gamma=2.0, alpha=0.25)
-
-    # Check if a previous checkpoint exists
-    if os.path.exists(checkpoint_path):
-        print(f"Loading weights from checkpoint: {checkpoint_path}")
-        # Load the model with the custom loss function
-        model = load_model(checkpoint_path, custom_objects={'loss': custom_loss})
-    else:
-        print("No checkpoint found. Starting training from scratch.")
-
-    # Compile the model with the custom loss function
-    model.compile(optimizer=Adam(learning_rate=1e-4), loss=custom_loss, metrics=['AUC', 'accuracy', 'Precision', 'Recall'])
-    model_checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=1, save_best_only=True)
-    
-    # Define the TensorBoard callback
-    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-    # Fit the model
-    history = model.fit(images, masks, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1, callbacks=[model_checkpoint, tensorboard_callback, lr_scheduler])
-    
-    return history
-
-
-# Using the functions
-history = train_unet(model, images, masks)
-
-
-# Define the loss functions
+# Define the Smooth L1 Loss Function
 def smooth_l1_loss(sigma=3.0):
     sigma_squared = sigma ** 2
     
@@ -161,41 +133,46 @@ def smooth_l1_loss(sigma=3.0):
             0.5 * sigma_squared * tf.pow(regression_diff, 2),
             regression_diff - 0.5 / sigma_squared
         )
-        # Sum over the last dimension to get the total loss for each bounding box
         regression_loss = tf.reduce_sum(regression_loss, axis=-1)
-        # Compute the mean loss over all the bounding boxes
         return tf.reduce_mean(regression_loss)
     
     return smooth_l1_loss_fixed
-
-
-def focal_loss(gamma=2.0, alpha=0.25):
-    def focal_loss_fixed(y_true, y_pred):
-        # Add epsilon to prevent log(0)
-        epsilon = tf.keras.backend.epsilon()
-        y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
-
-        cross_entropy = -y_true * tf.math.log(y_pred)
-        weights = alpha * tf.pow(1 - y_pred, gamma)
-        
-        # Reduce the last dimension to get the focal loss for each class
-        focal_loss = weights * cross_entropy
-        focal_loss = tf.reduce_sum(focal_loss, axis=-1)
-        # Compute the mean loss over all the classes
-        return tf.reduce_mean(focal_loss)
-    
-    return focal_loss_fixed
-
-
-# Compile the model with the custom loss and an optimizer
+# Compile the Model with the Custom Loss
 model.compile(optimizer='adam',
-              loss={
-                  'boxes_output': smooth_l1_loss(),
-                  'class_output': focal_loss()
-              },
-              metrics={'class_output': 'accuracy'})  # Assuming 'accuracy' is a suitable metric for your classification
+              loss=smooth_l1_loss(),  # Only use smooth L1 loss for bounding box predictions
+              metrics=['mean_squared_error'])  # Added a metric for bounding box prediction quality
+
+# Set up TensorBoard logging
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+# Set up checkpoints to save the model
+checkpoint_dir = "checkpoints/"
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+checkpoint_path = checkpoint_dir + "cp-{epoch:04d}.ckpt"
+checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True,
+    save_best_only=False  # Set to True to save only the best model based on validation loss
+)
+
+# Define a Learning Rate Schedule
+def scheduler(epoch, lr):
+    if epoch < 10:
+        return lr
+    elif epoch%10 == 0:
+        return lr * tf.math.exp(-0.1)
+    else:
+        return lr
+
+learning_rate_callback = LearningRateScheduler(scheduler)
 
 
-# Fit the model on the prepared dataset
-# The dataset should output a tuple of (images, {'boxes_output': boxes, 'class_output': class_labels})
-history = model.fit(dataset, epochs=10, steps_per_epoch=100)
+# Fit the Model on the Prepared Dataset
+# The dataset should output a tuple of (images, boxes)
+history = model.fit(
+    dataset,
+    epochs=500,
+    callbacks=[tensorboard_callback, checkpoint_callback, learning_rate_callback])  # Add the callbacks here
